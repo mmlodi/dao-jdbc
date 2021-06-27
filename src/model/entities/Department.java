764ac7a94948f8a2292b97dbf2cce0979eea648f
Mateus Lodi <mischellodi@gmail.com>
2021-06-27 16:50:49 -0300

    Import de itens do DB

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -29,7 +29,7 @@ public class Department implements Serializable {
         return name;
     }
 
-    public void setId( String name){
+    public void setName( String name){
         this.name = name;
     }
 
